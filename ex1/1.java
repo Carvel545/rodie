419f5a459c901aa39e6b237bddfc1fe2b2b9951d
Carvel545 <rtoualo@gmail.com>
2025-03-15 08:48:51 +0300

    Create 1.java

---
+++ ex1/1.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main (String[] args)
+       string str1 ="abc";
+       string str2 ="bc";
+
+       if (str1.endsWith(str2))  {
+          System.out.println(true);
+      } else {
+           System.out.println(false);
+      }
+   }
+}
