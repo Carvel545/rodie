a3d685b63d3b9c445822327421d308cf9282ef4e
Carvel545 <rtoualo@gmail.com>
2025-03-15 09:07:51 +0300

    Create 2.java

---
+++ ex1/2.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args)
+        String name = scanner.next();
+        System.out.println("Привет, " + name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase() + "!");
+   }
+}
